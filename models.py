a470a2cf93a761385287bf91247631b11f667f2b
Chaitanya Dwarampudi <125117351+ChaitanyaD404@users.noreply.github.com>
2026-04-08 19:07:01 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,14 @@
+from pydantic import BaseModel
+from typing import List
 
+class Observation(BaseModel):
+    dirt: List[float]
+    sun: float
+    battery: float
+    hour: int
+
+class Action(BaseModel):
+    action: str
+
+class Reward(BaseModel):
+    score: float
