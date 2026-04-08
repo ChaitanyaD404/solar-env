8cbff6fb7151be471801dbba56dab61996fe5cbd
Chaitanya Dwarampudi <125117351+ChaitanyaD404@users.noreply.github.com>
2026-04-08 19:06:22 +0530

    Add grading function to calculate score

--- graders.py
+++ graders.py
@@ -1,1 +1,13 @@
+def grade(total_reward, max_possible_reward):
+    if max_possible_reward == 0:
+        return 0.0
 
+    score = total_reward / max_possible_reward
+
+    # clamp between 0 and 1
+    if score > 1:
+        score = 1.0
+    if score < 0:
+        score = 0.0
+
+    return score
